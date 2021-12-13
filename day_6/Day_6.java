b39cabc76b100239e790c28c7451e96d43bf3c55
klerik <kl_erik@outlook.com>
2021-12-13 22:18:35 +0100

    Some clean-up

--- day_6/Day_6.java
+++ day_6/Day_6.java
@@ -3,12 +3,10 @@ package day_6;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class Day_6 {
   public static void main(String[] args) {
-    // File file = new File("day_6/test.txt");
     File file = new File("day_6/input.txt");
     ArrayList<Integer> fishes = new ArrayList<>();
 
@@ -22,45 +20,17 @@ public class Day_6 {
       e.printStackTrace();
     }
 
-    /*int days = 256;
-    int[] spawns = new int[days];
+    long[] timers = getTimers(256);
+    long sum = 0;
 
     for (int timer : fishes) {
-      calcSpawns4(spawns, timer);
-    }*/
-
-    /*for (int i = 0; i < spawns.length; i++) {
-      System.out.print(i + 1 + ":" + spawns[i] + ", ");
-    }*/
-
-    /*int sum = 0;
-
-    for (int i : spawns) {
-      sum += i;
-    }*/
-
-    //int spawns = 0;
-    /*int[] spawns = new int[7];
-    for (int i = 1; i < 7; i++) {
-      spawns[i] = calcSpawns(18, i);
-    }*/
-
-    /*HashMap<Integer, Integer> spawns = new HashMap<>();
-
-    int sum = 0;
-
-    for (int timer : fishes) {
-      if (spawns.containsKey(timer)) {
-        sum += spawns.get(timer);
-      } else {
-        spawns.put(timer, calcSpawns(256, timer));
-        sum += spawns.get(timer);
-      }
+      sum += timers[timer];
     }
 
-    System.out.println(sum * 5);*/
+    System.out.println(sum);
+  }
 
-    int days = 256;
+  private static long[] getTimers(int days) {
     long[] spawns = new long[days];
     long[] timers = new long[7];
     
@@ -78,76 +48,13 @@ public class Day_6 {
         spawns[i] = 3;
       }
 
+      // step cases
       for (int i = 9; i < spawns.length; i++) {
         spawns[i] = spawns[i - 7] + spawns[i - 9];
       }
 
       timers[j] = spawns[spawns.length - 1];
     }
-
-    long sum = 0;
-
-    for (int timer : fishes) {
-      sum += timers[timer];
-    }
-
-    System.out.println(sum);
-
-    /*
-    calcSpawns[days-timer, 7] + calcSpawns[days-timer, 9]
-    */
-  }
-
-  private static int calcSpawns(int days, int timer) {
-    int daysLeft = days - timer;
-
-    if (daysLeft > 0) {
-      return calcSpawns(daysLeft, 7) + calcSpawns(daysLeft, 9);
-    }
-
-    return 1;
-  }
-
-  private static void calcSpawns4(int[] spawns, int timer) {
-    for (int i = timer; i < spawns.length; i += 7) {
-      spawns[i]++;
-
-      calcSpawns4(spawns, i + 9);
-    }
-  }
-
-  private static int calcSpawns3(int days, int timer) {
-    int daysLeft = days - timer;
-
-    if (daysLeft > 0) {
-      int spawns = daysLeft % 7 == 0 ? daysLeft / 7 : daysLeft / 7 + 1;
-      int newSpawns = 0;
-
-      for (int s = 0; s < spawns; s++) {
-        newSpawns += calcSpawns3(daysLeft - 7 * s, 9);
-      }
-
-      return spawns + newSpawns;
-    }
-
-    return 0;
-  }
-
-  private static int calcSpawns2(int days, int timer) {
-    int spawns = 0;
-    int daysLeft = days - timer;
-
-    if (daysLeft > 0) {
-      spawns = daysLeft % 7 == 0 ? daysLeft / 7 : daysLeft / 7 + 1;
-      int newSpawns = 0;
-
-      for (int spawn = 0; spawn < spawns; spawn++) {
-        newSpawns += calcSpawns2(daysLeft - 7 * spawn, 9);
-      }
-
-      spawns += newSpawns;
-    }
-
-    return spawns;
+    return timers;
   }
 }
