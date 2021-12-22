caebc96d20b445c039276fc3f281c662ca736682
klerik <kl_erik@outlook.com>
2021-12-22 19:42:50 +0100

    Day 9 part 1 ok

--- day_9/Day_9.java
+++ day_9/Day_9.java
@@ -7,15 +7,44 @@ import java.util.Scanner;
 
 public class Day_9 {
   public static void main(String[] args) {
-    int[][] heightmap = parse("test.txt");
+    int[][] heightmap = parse("input.txt");
+    System.out.println(solve_p1(heightmap));
+  }
+
+  private static int solve_p1(int[][] heightmap) {
+    int sum = 0;
 
     for (int i = 0; i < heightmap.length; i++) {
       for (int j = 0; j < heightmap[i].length; j++) {
-        System.out.print(heightmap[i][j]);
+        if (lowPoint(heightmap, i, j)) {
+          sum += heightmap[i][j] + 1;
+        }
       }
-
-      System.out.println();
     }
+
+    return sum;
+  }
+
+  private static boolean lowPoint(int[][] heightmap, int i, int j) {
+    boolean lowPoint = true;
+
+    if (i < heightmap.length - 1) {
+      lowPoint &= heightmap[i][j] < heightmap[i + 1][j];
+    }
+
+    if (i > 0) {
+      lowPoint &= heightmap[i][j] < heightmap[i - 1][j];
+    }
+
+    if (j < heightmap[i].length - 1) {
+      lowPoint &= heightmap[i][j] < heightmap[i][j + 1];
+    }
+
+    if (j > 0) {
+      lowPoint &= heightmap[i][j] < heightmap[i][j - 1];
+    }
+
+    return lowPoint;
   }
 
   private static int[][] parse(String string) {
